830f9f30605de6bc72502b442b41dd9c36304659
Andres Negrette <andresdngtt@gmail.com>
2022-11-10 00:27:07 -0300

    se corrigen errores luego del merge

--- dapd-proyecto-final/src/app/app.module.ts
+++ dapd-proyecto-final/src/app/app.module.ts
@@ -7,7 +7,6 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { IonicStorageModule } from '@ionic/storage-angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { IonicStorageModule } from '@ionic/storage-angular';
 import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({

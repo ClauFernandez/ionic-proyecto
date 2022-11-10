5ca851f523336d408ac925307f6006ac59d32660
Fernando Bártoli <54591089+fdbartoli@users.noreply.github.com>
2022-11-09 21:03:42 -0300

    Update app-routing.module.ts
    correción PR

--- dapd-proyecto-final/src/app/app-routing.module.ts
+++ dapd-proyecto-final/src/app/app-routing.module.ts
@@ -2,13 +2,9 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { LoginGuard } from './auth/guards/login.guard';
 import { DashboardGuard } from './dashboard/guards/dashboard.guard';
-import { RegistrosComponent } from './components/registros/registros.component';
+
 
 const routes: Routes = [
-  {
-    path: 'registros',
-    component: RegistrosComponent,
-  },
   {
     path: '',
     redirectTo: 'dashboard',
@@ -29,10 +25,6 @@ const routes: Routes = [
     redirectTo: 'login',
     pathMatch: 'full'
   },
-  {
-    path: 'registros',
-    component: RegistrosComponent,
-  }
 ];
 
 @NgModule({

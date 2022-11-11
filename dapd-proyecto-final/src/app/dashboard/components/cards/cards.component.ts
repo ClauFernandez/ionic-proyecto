66fde145fc35a48d449fbd41739cf85977a0b01a
Clau <claufernandez012@gmail.com>
2022-11-11 12:55:58 -0300

    funcionalidades de la cards

--- dapd-proyecto-final/src/app/dashboard/components/cards/cards.component.ts
+++ dapd-proyecto-final/src/app/dashboard/components/cards/cards.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Registro } from 'src/app/shared/models/models';
+import { RegistroService } from 'src/app/shared/services/registro.service';
 
 @Component({
   selector: 'app-cards',
@@ -6,7 +8,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cards.component.scss'],
 })
 export class CardsComponent implements OnInit {
-  constructor() {}
+  constructor(private registroService: RegistroService) {}
+  registro: Registro[];
+  gastos: number = 0;
+  ingresos: number = 0;
+  balance: number = 0;
 
-  ngOnInit() {}
+  registrosPositivos() {
+    let suma: number = 0;
+    this.registro = this.registroService.registros;
+    for (let i: number = 0; i < this.registro.length; i++) {
+      if (this.registro[i].ingreso) {
+        suma = suma + this.registro[i].monto;
+      }
+    }
+    return suma;
+  }
+
+  registrosNegativos() {
+    let suma: number = 0;
+    this.registro = this.registroService.registros;
+    for (let i: number = 0; i < this.registro.length; i++) {
+      if (!this.registro[i].ingreso) {
+        suma = suma + this.registro[i].monto;
+      }
+    }
+    return suma;
+  }
+
+  ngOnInit() {
+    this.gastos = this.registrosNegativos();
+    this.ingresos = this.registrosPositivos();
+    this.balance = this.registrosPositivos() - this.registrosNegativos();
+  }
+
+  ngDoCheck() {
+    this.gastos = this.registrosNegativos();
+    this.ingresos = this.registrosPositivos();
+    this.balance = this.registrosPositivos() - this.registrosNegativos();
+  }
 }
